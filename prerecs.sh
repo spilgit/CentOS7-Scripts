dbc44399467fee5a25eb5f88e882ab2c77073f47
KHMF Technologies <spilgit@users.noreply.github.com>
2016-10-11 14:58:38 -0400

    Create prerecs.sh

---
+++ prerecs.sh
@@ -0,0 +1,7 @@
+# Taken from -> http://www.tecmint.com/things-to-do-after-minimal-rhel-centos-7-installation/
+# Using LINODE for testing purposes
+nmcli d
+nmtui
+cd /etc/sysconfig/network-scripts/
+ls -lha
+ifconfig
