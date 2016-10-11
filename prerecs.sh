fe1ebf8f0c0dc36370650952a9ef7ac193035cb2
KHMF Technologies <spilgit@users.noreply.github.com>
2016-10-11 16:53:06 -0400

    Update prerecs.sh

--- prerecs.sh
+++ prerecs.sh
@@ -8,3 +8,6 @@ reboot
 ping -c 4 google.com
 ifconfig
 wget https://sourceforge.net/projects/webadmin/files/webmin/1.820/webmin-1.820.tar.gz
+# Prevent DDos attack 1
+iptables -I INPUT -p tcp --dport 22 -m state --state NEW -m hashlimit --hashlimit-name limitssh --hashlimit-above 1/minute --hashlimit-burst 10 --hashlimit-mode srcip --hashlimit-srcmask 28 -j DROP
+
