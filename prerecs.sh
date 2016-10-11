5966f3cb2bd797b32acd22eb6b6deb8e27d3100f
KHMF Technologies <spilgit@users.noreply.github.com>
2016-10-11 16:51:35 -0400

    Update prerecs.sh

--- prerecs.sh
+++ prerecs.sh
@@ -4,5 +4,7 @@ nmcli d
 nmtui
 cd /etc/sysconfig/network-scripts/
 ls -lha
+reboot
+ping -c 4 google.com
 ifconfig
 wget https://sourceforge.net/projects/webadmin/files/webmin/1.820/webmin-1.820.tar.gz
